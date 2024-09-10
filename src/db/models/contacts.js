d9bdac0c366400d2599a52c3645b90a72f356796
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-09-10 22:32:26 +0300

    hw 2

---
+++ src/db/models/contacts.js
@@ -0,0 +1,41 @@
+// src/db/models/student.js 
+
+import { model, Schema } from 'mongoose';
+
+const contactSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+        },
+        isFavourite: {
+            type: Boolean,
+            default: false,
+        },
+
+        contactType: {
+            type: String,
+            enum: ['work', 'home', 'personal'],
+            required: true,
+            default: 'personal',
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    },
+);
+
+
+
+export const ContactsCollection = model('contacts', contactSchema);
