b34a32c7ace587d5962f51d3754deb7fc1ea1ae6
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-10-08 19:29:12 +0300

    updated

--- src/validation /validationContacts.js
+++ src/validation /validationContacts.js
@@ -1,10 +1,10 @@
 import Joi from 'joi';
-import { contactTypeList } from '../constants/index.js';
+import { contactTypeList, phoneNumberRegexp } from '../constants/index.js';
 
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
-    phoneNumber: Joi.string().min(3).max(20).required().messages({
-        'string.pattern.base': 'Phone number must be in the format 380XXXXXXXXX',
+    phoneNumber: Joi.string().min(3).max(20).pattern(phoneNumberRegexp).required().messages({
+        'string.pattern.base': 'Phone number must be in the format +380XXXXXXXXX',
     }),
     email: Joi.string().email().min(3).max(30),
     isFavorite: Joi.boolean(),
@@ -14,8 +14,8 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20),
-    phoneNumber: Joi.string().min(3).max(20).messages({
-        'string.pattern.base': 'Phone number must be in the format 380XXXXXXXXX',
+    phoneNumber: Joi.string().min(3).max(20).pattern(phoneNumberRegexp).messages({
+        'string.pattern.base': 'Phone number must be in the format +380XXXXXXXXX',
     }),
     email: Joi.string().email().min(3).max(20),
     isFavorite: Joi.boolean(),
